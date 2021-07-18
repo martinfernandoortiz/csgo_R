5afd9bb6df42c11af6248304eea85060f11c544e
martinfernandoortiz <martinfernandoortiz@gmail.com>
2021-07-17 21:12:39 -0300

    Actualizacion Graficos, dos tabs

--- intro.R
+++ intro.R
@@ -1,28 +1,31 @@
 #https://adsoncostanzifilho.github.io/blog/2021/03/25/csgo-package/
 library(tidyverse)
-install.packages("CSGo")
+#install.packages("CSGo")
 library(CSGo)
 library(dplyr)
 library(stringr)
-
+library(highcharter)
 #76561199036069511
-csgo_Stats <- get_stats_friends(api_key = '8FEEC193DD806E291A104B62C8337B3B', user_id = '76561199036069511') #Dani
+csgo_Stats1 <- get_stats_friends(api_key = '8FEEC193DD806E291A104B62C8337B3B', user_id = '76561198018191299') #Dani
+csgo_Stats <- get_stats_friends(api_key = '8FEEC193DD806E291A104B62C8337B3B', user_id = '76561199036069511') #yo
 
 #dani_stats <- get_stats_user(api_key = '8FEEC193DD806E291A104B62C8337B3B', user_id = '76561198018191299')
 
-ortiX_stats <- get_stats_user(api_key = '8FEEC193DD806E291A104B62C8337B3B', user_id = '76561199036069511')
-ortiX <-  csgo_api_stats(api_key = '8FEEC193DD806E291A104B62C8337B3B', user_id = '76561199036069511')
+#ortiX_stats <- get_stats_user(api_key = '8FEEC193DD806E291A104B62C8337B3B', user_id = '76561199036069511')
+#ortiX <-  csgo_api_stats(api_key = '8FEEC193DD806E291A104B62C8337B3B', user_id = '76561199036069511')
 
 #vignette("auth", package = "CSGo")
 #vignette("usecase", package = "CSGo") 
 
 
 df <- as.data.frame(csgo_Stats$friends_stats)
-df <- rbind(df,ortiX_stats)
+df1 <- as.data.frame(csgo_Stats1$friends_stats) %>% filter(player_name=='0rT1x')
+df <- rbind(df,df1)
 
-df %>% group_by(category, type) %>% count()
+#df %>% group_by(category, type) %>% count()
 
-
+df[is.na(df)] <- 0
+df <- df %>% mutate(value=as.integer(value))
 #PERFORMANCE WINS POR MAPA
 
 
@@ -32,6 +35,42 @@ stat <- df %>%  filter(type=='stat' & category=='stats') %>% select(c(1,2,7)) %>
 performance <- df %>%  filter(category=='performance') %>% select(c(1,2,7)) %>% pivot_wider(names_from = name, values_from = value)
 last <- df %>%  filter(category=='last match') %>% select(c(1,2,7)) %>% pivot_wider(names_from = name, values_from = value)
 
-last <- df %>% filter(str_sub(name,1,4) =='last')
-last
+
 df %>% group_by(category,type) %>% count()
+
+
+#TIEMPO JUGADO
+tiempoJugado <- hchart(stat, "column", hcaes(x = player_name, y = total_time_played))
+
+#SHOTS/KILLS
+performance <- performance %>% mutate(efectividadShots= as.integer(total_kills)/as.integer(total_shots_fired)) %>% arrange(-efectividadShots)
+efectividad <- hchart(performance, "column", hcaes(x = player_name, y = efectividadShots))
+#hits/shots
+performance <- performance %>% mutate(efectividadhits= as.integer(total_shots_hit)/as.integer(total_shots_fired)) %>% arrange(-efectividadhits)
+hits <- hchart(performance, "column", hcaes(x = player_name, y = efectividadhits))
+
+#GANADOS/JUGADoS
+stat <- stat %>% mutate(efectividadPartidos= as.integer(total_matches_won)/as.integer(total_matches_played)) %>% arrange(-efectividadPartidos)
+
+matchsWon <- hchart(stat, "column", hcaes(x = player_name, y = efectividadPartidos))
+matchsWon
+# MVPS/ MATCH
+stat <- stat %>% left_join(performance[,c(1:7,38:40,71,72)], by="player_name") %>% 
+  mutate(mvp= as.integer(total_mvps)/as.integer(total_rounds_played)) %>% arrange(-mvp)
+
+mvp <- hchart(stat, "column", hcaes(x = player_name, y = mvp))
+
+# CUCHI/KILLS
+tiempoJugado <- hchart(stat, "column", hcaes(x = player_name, y = total_time_played))
+
+#DEADS/MATCHS
+tiempoJugado <- hchart(stat, "column", hcaes(x = player_name, y = total_time_played))
+
+#DAMAGE
+stat <- stat %>% mutate(damage= as.integer(total_damage_done)/as.integer(total_rounds_played)) %>% arrange(-damage)
+damage <- hchart(stat, "column", hcaes(x = player_name, y = damage))
+
+#Score
+stat <- stat %>% mutate(score= as.integer(total_contribution_score)/as.integer(total_matches_played)) %>% arrange(-score)
+
+total_contribution_score total_rounds_played
--- stats/app.R
+++ stats/app.R
@@ -1,13 +1,43 @@
-#library(shinydashboard)
+library(shiny)
 
+library(shinydashboard)
+library(shinyWidgets)
+library(tidyverse)
+library(leaflet)
+library(RColorBrewer)
+library(shinyWidgets)
+library(readr)
+library(htmltools)
+library(data.table)
 #data
 total_matches_played <- df %>% filter(name=='total_matches_played') 
 total_matches_played <- sum(total_matches_played$value)
+nombres <- df %>% group_by(player_name) %>% count()
 
 ui <- dashboardPage(
-    dashboardHeader(title = "Info boxes"),
-    dashboardSidebar(),
+    dashboardHeader(title = "CSGO"),
+    dashboardSidebar(
+        sidebarMenuOutput("menu"),
+        sidebarMenuOutput("menu2"),
+
+        pickerInput(
+            inputId = "nombresset",
+            label = "Selecciona un jugador",
+            choices = unique(nombres$player_name),
+            selected = nombres[1,1],
+            multiple = FALSE,
+            options = list(
+                `selected-text-format`= "count",
+                `count-selected-text` = "{0} models choosed (on a total of {1})"
+            )
+        )
+
+    ),
+    
     dashboardBody(
+        tabItems(
+            #################### TAB DASHBOARD
+        tabItem(tabName = "dashboard",
         fluidRow(
             
             infoBoxOutput("partidos", width = 3),
@@ -30,7 +60,7 @@ ui <- dashboardPage(
         box(
             title = "Histogram", status = "primary", solidHeader = TRUE,
             collapsible = TRUE,
-            plotOutput("plot3", height = 250)
+            highchartOutput("plot1")
         ),
         
         box(
@@ -41,16 +71,53 @@ ui <- dashboardPage(
         )
         
         )
+        ),
+        
+        #################### TAB WIDGETS
+        
+    tabItem(tabName = "widgets",
+            fluidRow(
+                box(
+                        title = "Ganados/Jugados", status = "primary", solidHeader = TRUE,
+                        collapsible = TRUE,
+                        highchartOutput("plot2")
+                    )
+            )
+        )
+        )
     )
 )
-
+################################################################################################################################
 server <- function(input, output) {
-    
 
-    # Same as above, but with fill=TRUE
-    output$partidos <- renderInfoBox({
+   # df_reactive <- reactive({
+    #    df %>%
+    #        filter(player_name == input$nombresset) %>%
+    #        arrange(nombresset) 
+    #})
+    
+########## MENU
+    output$menu <- renderMenu({
+        sidebarMenu(
+            menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard"))
+                    )
+    })
+    output$menu2 <- renderMenu({
+        sidebarMenu(
+            menuItem("Widgets", icon = icon("th"), tabName = "widgets")
+        )
+    })
+    
+########## BODY
+    ###### TAB 1
+        
+      output$partidos <- renderInfoBox({
+          a <- df %>%
+              filter(player_name == input$nombresset)
+          a <- a %>% filter(name=='total_matches_played')  
+          a<- sum(a$value)
         infoBox(
-            "Matchs jugados", total_matches_played, icon = icon("list"),
+            "Matchs jugados", a, icon = icon("list"),
             color = "purple", fill = TRUE
         )
     })
@@ -112,6 +179,13 @@ server <- function(input, output) {
             "Combates con Facas", total_matches_played, icon = icon("list"),
             color = "purple", fill = TRUE
         )
+    })
+    output$plot1 <- renderHighchart({
+        hchart(stat, "column", hcaes(x = player_name, y = total_time_played))
+    })
+    
+    output$plot2 <- renderHighchart({
+        hchart(stat, "column", hcaes(x = player_name, y = total_time_played))
     })
 }
 
